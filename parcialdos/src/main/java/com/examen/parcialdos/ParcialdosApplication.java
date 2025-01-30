27ec35e06bbd62b6ae797a8b90d9f0368c6258d4
Anthony Quishpe <aequishpe2@espe.edu.ec>
2025-01-29 19:05:41 -0500

    ex2
    ex2

---
+++ parcialdos/src/main/java/com/examen/parcialdos/ParcialdosApplication.java
@@ -0,0 +1,13 @@
+package com.examen.parcialdos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialdosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialdosApplication.class, args);
+	}
+
+}
